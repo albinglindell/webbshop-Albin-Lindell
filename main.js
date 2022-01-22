9e344014bf4db1a042aa833ed71b7e1d5bab8382
Albin Lindell <albinglindell98@gmail.com>
2022-01-22 15:08:00 +0100

    changed the whole thing

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 fetch('https://fakestoreapi.com/products')
             .then(res=>res.json())
             .then(json=>{console.log(json)
+                let buyItems = []
+                let cart = document.querySelector(".cart")
+                let checkoutDiv = document.querySelector(".checkout-div")
+                let container = document.querySelector(".container")
                 json.forEach(e => {
-                let produkter = document.querySelector(".produkter")
+                    
+                    let produkter = document.querySelector(".produkter")
+                let span1 = document.querySelector("#quantity")
 
                 let prodDiv = document.createElement("div")
                 prodDiv.classList.add("produkten")
@@ -14,17 +20,34 @@ fetch('https://fakestoreapi.com/products')
                 let prodP = document.createElement("p")
                 let info = document.createElement("p")
                 let prodPrice = document.createElement("h3")
+                let buyBtn = document.createElement("button")
 
                 info.innerText = e.description
                 prodP.innerText = e.title
                 prodImg.src=e.image;
                 prodPrice.innerText=`${e.price}$`
+                buyBtn.innerText="Buy now"
                 
                 infocard.appendChild(info)
                 prodDiv.appendChild(prodImg)
                 prodDiv.appendChild(prodP)
                 produkter.appendChild(prodDiv)
                 prodDiv.appendChild(prodPrice)
-                });
-            
+                prodDiv.appendChild(buyBtn)
+               
+
+                buyBtn.addEventListener("click", () =>{
+                    buyItems.push(e)
+                    span1.innerText++
+                })
+
+                
+            });
+            cart.addEventListener("click",() =>{
+                container.style.display="none"
+                const checkout = document.createElement("div")
+                checkout.classList.add=("checkout")
+                checkoutDiv.style.zIndex="10"
+                checkoutDiv.appendChild(checkout)
+            })
             })
